462d7cd2a1141b7f3f919ec6af4c0f7d75d7af8c
Zoozoo00 <ranjnaganeshram@gmail.com>
2017-08-06 14:48:39 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,6 +9,18 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+app.get('/article-zero',function(req,res){
+    res.send('Article zero')
+});
+
+app.get('/article-one',function(req,res){
+    res.send('Article one')
+});
+
+app.get('/article-two',function(req,res){
+    res.send('Article two')
+});
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
